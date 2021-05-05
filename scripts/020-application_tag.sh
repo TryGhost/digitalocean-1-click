8b7762db02a1a1b7a54b2dd2e706954a6684a877
Daniel Lockyer <hi@daniellockyer.com>
2021-05-05 10:01:39 +0100

    Updated application tag for Ghost to Ubuntu 20
    no issue
    
    - this was missed from the previous commit [1] and accurately reflects
      the bump in OS version
    
    [1]: https://github.com/TryGhost/digitalocean-1-click/commit/a75384fff181a99789915cf4e57fe3e7cf329b21

--- scripts/020-application_tag.sh
+++ scripts/020-application_tag.sh
@@ -10,8 +10,8 @@ cat >> /var/lib/digitalocean/application.info <<EOM
 appiication_name="Ghost"
 build_date="${build_date}
 distro=Ubuntu
-distro_release=18.04
-distro_codename=bionic
+distro_release=20.04
+distro_codename=focal
 distro_arch=amd64
 application_version="${version}"
 EOM
