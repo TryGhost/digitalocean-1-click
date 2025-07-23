736d2db78f660bf67f74a404b0256ebab6c6af7a
Sam Lord <hello@samlord.me>
2025-07-23 12:16:31 +0100

    Updated application tag

--- scripts/020-application_tag.sh
+++ scripts/020-application_tag.sh
@@ -10,8 +10,8 @@ cat >> /var/lib/digitalocean/application.info <<EOM
 appiication_name="Ghost"
 build_date="${build_date}
 distro=Ubuntu
-distro_release=20.04
-distro_codename=focal
+distro_release=24.04
+distro_codename=noble
 distro_arch=amd64
 application_version="${version}"
 EOM
