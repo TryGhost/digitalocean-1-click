d1bb97f3af9bc0e1b677db1ca8adc2ded8cdaa40
Sam Lord <hello@samlord.me>
2025-07-24 16:13:30 +0100

    Updated to Node 22 / Ubuntu 24.04 (#7)
    * Updated to Node 22 / Ubuntu 24.04
    
    Tidied up the allowed operating systems and switched to the recommended
    method for installing Node
    
    * Updated application tag
    
    * Replaced curl|bash with manual steps

--- scripts/010-nodejs.sh
+++ scripts/010-nodejs.sh
@@ -3,17 +3,15 @@
 ##
 ## vi: syntax=sh expandtab ts=4
 
-curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-
 # Replace with the branch of Node.js or io.js you want to install: node_6.x, node_8.x, etc...
 VERSION=${NODE_VERSION}
-# The below command will set this correctly, but if lsb_release isn't available, you can set it manually:
-# - For Debian distributions: jessie, sid, etc...
-# - For Ubuntu distributions: xenial, bionic, etc...
-# - For Debian or Ubuntu derived distributions your best option is to use the codename corresponding to the upstream release your distribution is based off. This is an advanced scenario and unsupported if your distribution is not listed as supported per earlier in this README.
-DISTRO="$(lsb_release -s -c)"
-echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+
+# Create a directory for the new repository's keyring, if it doesn't exist
+mkdir -p /etc/apt/keyrings
+# Download the new repository's GPG key and save it in the keyring directory
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+# Add the new repository's source list with its GPG key for package verification
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${VERSION}.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 # Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
