329ef309fbe0856dcec42cfa520fe9a295643493
Sam Lord <hello@samlord.me>
2025-07-24 12:05:07 +0100

    Replaced curl|bash with manual steps

--- scripts/010-nodejs.sh
+++ scripts/010-nodejs.sh
@@ -3,13 +3,15 @@
 ##
 ## vi: syntax=sh expandtab ts=4
 
-curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-
 # Replace with the branch of Node.js or io.js you want to install: node_6.x, node_8.x, etc...
 VERSION=${NODE_VERSION}
 
-# Add Node.js repository
-curl -fsSL https://deb.nodesource.com/setup_$VERSION.x | bash -
+# Create a directory for the new repository's keyring, if it doesn't exist
+mkdir -p /etc/apt/keyrings
+# Download the new repository's GPG key and save it in the keyring directory
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+# Add the new repository's source list with its GPG key for package verification
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${VERSION}.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 # Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
